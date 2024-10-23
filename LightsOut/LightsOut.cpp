b8d5a39e85027425abf5413b589270454d15223f
Pixelz22 <tljohnson1225@gmail.com>
2024-10-23 15:10:34 -0400

    Repo and CMake setup

---
+++ LightsOut/LightsOut.cpp
@@ -0,0 +1,12 @@
+﻿// LightsOut.cpp : Defines the entry point for the application.
+//
+
+#include "LightsOut.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
