f8e099dc724d39a897d503d610327d702cfdf050
Pixelz22 <tljohnson1225@gmail.com>
2025-05-08 14:02:11 -0400

    we're making it out of the toolchain with this one, boys

---
+++ src/LightsOut.cpp
@@ -0,0 +1,16 @@
+﻿// LightsOut.cpp : Defines the entry point for the application.
+//
+
+#include "LightsOut.h"
+#include <SDL/SDL.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Press enter to quit...";
+	int a;
+	cin >> a;
+
+	return 0;
+}
